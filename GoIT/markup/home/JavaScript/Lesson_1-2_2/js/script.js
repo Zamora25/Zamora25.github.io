1840f4db730d75d01c21397634d42c5506572ae4
Zamora25 <zamorsky@list.ru>
2015-12-27 17:01:45 +0200

    js

--- GoIT/markup/home/JavaScript/Lesson_1-2_2/js/script.js
+++ GoIT/markup/home/JavaScript/Lesson_1-2_2/js/script.js
@@ -3,13 +3,15 @@
 
 	for (var i = 0; i < 5; i++) {
 
-		var name = prompt('введите имя пользователя')
+		var name = prompt('введите имя пользователя');
+		name = name.toLowerCase();
 		arr[i] = name;
 	};
 
 	console.log(arr);
 
 	var userName = prompt('введите имя пользователя');
+	name = name.toLowerCase();
 	var flag = false;
 
 	for (var i = 0; i < 5; i++) {
